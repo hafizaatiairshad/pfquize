e36cea962f0409d0f379ec29e4f83e49cd30fa1c
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-06 16:01:23 +0500

    Unused Deleted

--- Qno 3.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main ( ) {
-	cout<<"C language is a powerfull language: "<<endl;
-	cout<<"Qno 1 Clanguage is a powerfull language: "<<endl;
-	return 0;
-}
