605a8ba00aa52bf58e02d2547f3dab470d14c380
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-07 00:00:56 +0500

    Assignment Qno5

---
+++ Qno6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ( ) {
+    int num1, num2;
+    cout<<"Enter the value of numbers to find sum and product: "<<num1<<num2<<endl;
+    cin>>num1>>num2;
+    int sum = num1 + num2;
+    cout<<"The sum of numbers is: "<<sum<<endl;
+    int product = num1 * num2;
+    cout<<"The Product of numbers is: "<<product<<endl;
+    return 0;
+}
