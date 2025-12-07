d857a07ce9b073edd71a9935a871cd8f664545d2
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-07 18:19:38 +0500

    Assignment Qno14

---
+++ Qno14.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+    float mm = 12.3,inches;
+    inches = 25.4 * mm;
+    cout<<"Millimeters in inches are: "<<inches<<endl;
+    return 0;
+}
