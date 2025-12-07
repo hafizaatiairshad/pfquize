df15d40f22148c6940c6bb7075815deb57b6776e
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-07 14:36:08 +0500

    Assignment Qno10

---
+++ Qno10.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main ( ) {
+    float miles = 2.5;
+    float kilometers = 1.609 * miles;
+    cout<<"Miles into Kilometers is "<<kilometers<<endl;
+    return 0;
+}
+
