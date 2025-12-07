96ee89ecf02f254ff33dc349475060ec04aa962a
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-07 14:01:08 +0500

    Assignment Qno9

---
+++ Qno9.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+    int num1,num2,num3,num4;
+    cout<<"Enter the value of  four Numbers: "<<num1<<" "<<num2<<" "<<num3<<" "<<num4<<endl;
+    cin>>num1>>num2>>num3>>num4;
+    int maximum = num1;
+    
+    if( num2 > maximum) 
+      maximum = num2;
+      if (num3 > maximum ) 
+       maximum = num3;
+      
+	  if(num4 > maximum) 
+     maximum = num4;	
+   cout<<"Maximum Number is: "<<maximum<<endl;
+    
+   
+    return 0;
+}
