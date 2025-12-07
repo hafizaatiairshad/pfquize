e3aff3c755c0724a9ef6c5c657ae0809b798401c
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-07 14:50:18 +0500

    Assignment Qno11

---
+++ Qno11.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+    int a = 4, b = 4;
+    int total = a + b;
+    int average = total / 2;
+    cout<<"Average value of a and b is: "<<average<<endl;
+    return 0;
+}
