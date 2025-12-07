f4026b90ee96063370ba53bcbe438a0e41eaa870
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-07 18:36:32 +0500

    Assignment Qno16

---
+++ Qno 16.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+    int a , b , c;
+    a = b = c = 3;
+    int product;
+    product = a * b * c;
+    cout<<"Product of a, b and c values is "<<product<<endl;
+    return 0;
+}
