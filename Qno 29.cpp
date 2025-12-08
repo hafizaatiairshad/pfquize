7feafd47741a6923f6642a00c167abd49f57a0ff
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-08 23:56:05 +0500

    Assignment Qno29

---
+++ Qno 29.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+    int n;
+    cout<<"Enter Number: ";
+    cin>>n;
+    if(n > 100) {
+        cout<<"Number "<<n<< "is greater than 100. "<<endl;
+    }
+    return 0;
+}
