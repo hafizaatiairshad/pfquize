281bb7564a4283492760c678b9b2f34def938201
hafizaatiairshad <hafizaatiairshad@gmail.com>
2025-12-08 18:25:17 +0500

    Assignment Qno19

---
+++ Qno19.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ( ) {
+    int year = 19;
+    int months = 12 * year;
+    cout<<" Age in months "<<months<<endl;;
+    int days = 365 * year;
+    cout<<"Age in days "<<days<<endl;
+    return 0;
+}
